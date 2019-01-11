0835666d8cfcda612a87d149bee197bf206f2166
olystrel <46057808+olystrel@users.noreply.github.com>
2019-01-11 22:22:16 +0100

    easy level
    Решила 3 задания уровня easy

---
+++ lesson01_easy.py
@@ -0,0 +1,26 @@
+_author_='Olga_Strelenko'
+
+#Задача1
+
+number = int(input(' Введите число '))
+for i in str(number):
+    	print(i)
+
+#Задача2
+
+answer_user= input('Введите данные ' )
+answer_user1= input('Введите данные1 ')
+answer_change = ''
+answer_change = answer_user
+answer_user=answer_user1
+answer_user1= answer_change
+print(answer_user, answer_user1)
+
+
+#Задача3
+age=int(input('Введите свой возраст '))
+if age >= 18:
+    print('Доступ разрешен')
+else:
+    print('Извините, пользование данным ресурсом только с 18 лет')
+
